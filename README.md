0072d77c782b6eee4d963d9fdff2d8c10c838d11
Sergei Soletskii <serey19992@yandex.ru>
2026-08-15 23:17:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![icon of test](https://github.com/TokarevSilary//js_poppup/actions/workflows/web.yml/badge.svg)
+
+## Puppeteer
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import { RenderPage } from "../components/render/RenderPage";
+
+const renderPage = new RenderPage(".container");
+
+renderPage.render();
